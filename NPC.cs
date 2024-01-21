d8cbdfb8c66d77dc2e72a8d64f507d603a4714a1
serega338 <123870074+serega338@users.noreply.github.com>
2024-01-21 14:39:23 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //Здоровье NPC
+    public int health = 5;
+
+    //Уровень NPC 
+    public int level = 1;
+
+    //Скорость NPC
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = health + level;
+        print("Здоровье:" + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        newPosition.y += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
